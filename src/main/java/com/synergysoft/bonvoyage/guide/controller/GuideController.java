430a5f1e38888510325c91a4d9fd3568671d69e0
sujannoh <nohsoojin6@gmail.com>
2024-09-28 14:47:37 +0900

    guide 환경설정 세팅

--- src/main/java/com/synergysoft/bonvoyage/guide/controller/GuideController.java
+++ src/main/java/com/synergysoft/bonvoyage/guide/controller/GuideController.java
@@ -1,5 +1,19 @@
 package com.synergysoft.bonvoyage.guide.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import com.synergysoft.bonvoyage.guide.model.service.GuideService;
+
+@Controller
 public class GuideController {
+	private static final Logger logger = LoggerFactory.getLogger(GuideController.class);
+	
+	@Autowired
+	private GuideService guideService;
+	
+	
 
 }
