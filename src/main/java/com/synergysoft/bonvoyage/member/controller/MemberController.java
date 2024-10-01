94d9607100a0a3afd5c865b15c0a1e65eaf95127
Jeongeunji2002 <ejjung02@beeu.co.kr>
2024-10-01 20:19:13 +0900

    회원 기능 작성(관리자 페이지 구현 준비) (2024-10-01)

--- src/main/java/com/synergysoft/bonvoyage/member/controller/MemberController.java
+++ src/main/java/com/synergysoft/bonvoyage/member/controller/MemberController.java
@@ -18,64 +18,66 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.synergysoft.bonvoyage.common.Paging;
 import com.synergysoft.bonvoyage.member.model.dto.Member;
 import com.synergysoft.bonvoyage.member.model.service.MemberService;
 
 @Controller
 public class MemberController {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(MemberController.class);
-	
+
 	@Autowired
 	private MemberService memberService;
-	
+
 	@Autowired
 	private BCryptPasswordEncoder bcryptPasswordEncoder;
-	
+
 	// 로그인 페이지 출력 | 2024. 09. 28 작성 및 테스트 성공
 	@RequestMapping(value = "loginPage.do", method = { RequestMethod.GET, RequestMethod.POST })
 	public String moveLoginPage() {
 		return "member/loginPage";
 	}
-	
+
 	// 회원가입 페이지 출력 | 2024. 09. 28 작성 및 테스트 성공
 	@RequestMapping("enrollPage.do")
 	public String moveEnrollPage() {
 		return "member/enrollPage";
 	}
-	
+
 	// 소셜 로그인 페이지 출력 | 2024. 09. 28 작성 및 테스트 성공
 	@RequestMapping(value = "socialLoginPage.do", method = { RequestMethod.GET, RequestMethod.POST })
 	public String moveSocialLoginPage() {
 		return "member/socialLoginPage";
 	}
-	
+
 	// 소셜 회원가입 페이지 출력 | 2024. 09. 28 작성 및 테스트 성공
 	@RequestMapping(value = "socialEnrollPage.do", method = { RequestMethod.GET, RequestMethod.POST })
 	public String moveSocialEnrollPage() {
 		return "member/socialEnrollPage";
 	}
-	
+
 	// 아이디 찾기 페이지 출력 | 2024. 09. 28 작성 및 테스트 성공
 	@RequestMapping(value = "idSearchPage.do", method = { RequestMethod.GET, RequestMethod.POST })
 	public String moveIDSearchPage() {
 		return "member/idSearch";
 	}
-	
+
 	// 비밀번호 찾기 페이지 출력 | 2024. 09. 28 작성 및 테스트 성공
 	@RequestMapping(value = "pwSearchPage.do", method = { RequestMethod.GET, RequestMethod.POST })
 	public String movePWSearchPage() {
 		return "member/pwSearch";
 	}
-	
+
 	// 내 정보 조회 페이지 출력 | 2024. 09. 28 작성 및 테스트 성공
 	@RequestMapping(value = "myinfo.do")
 	public String moveMyInfoPage(@RequestParam("memId") String memId, Model model) {
 		logger.info("myinfo.do : " + memId);
-		
+
 		Member member = memberService.selectMyinfo(memId);
-		
+
 		if (member != null) {
 			model.addAttribute("member", member);
 			return "member/myinfo/myinfo";
@@ -84,13 +86,13 @@ public class MemberController {
 			return "common/error";
 		}
 	} // 내 정보 조회 페이지 출력
-	
+
 	// 내가 쓴 댓글(가이드게시판) 페이지 출력 | 2024. 09. 28 작성 및 테스트 성공
 	@RequestMapping(value = "myinfo/comment/guideBoard.do")
 	public String moveMyinfoCommentGuideBoard() {
 		return "member/myinfo/comment/guideBoard";
 	}
-	
+
 	// 내가 쓴 댓글(경로게시판) 페이지 출력 | 2024. 09. 28 작성 및 테스트 성공
 	@RequestMapping(value = "myinfo/comment/routeBoard.do")
 	public String moveMyinfoCommentRouteBoard() {
@@ -99,41 +101,41 @@ public class MemberController {
 
 	// 관리자 : 회원 목록 조회 페이지 출력 | 2024. 09. 28 작성 및 테스트 성공
 	@RequestMapping(value = "admin/memberList.do")
-	public String moveMemberList() {
+	public String moveMemberList(ModelAndView mv ) {
 		return "admin/memberList";
 	}
-	
+
 	// 관리자 : 회원 상세 조회 페이지 출력 | 2024. 09. 28 작성 및 테스트 성공
 	@RequestMapping(value = "admin/memberDetail.do")
 	public String moveMemberDetail() {
 		return "admin/memberDetail";
 	}
-	
+
 	// 로그인 기능 | 2024. 09. 28 작성 및 테스트 성공
-	@RequestMapping(value = "login.do", method=RequestMethod.POST)
+	@RequestMapping(value = "login.do", method = RequestMethod.POST)
 	public String loginMethod(Member member, HttpSession session, SessionStatus status, Model model) {
 		logger.info("login : " + member);
-		
+
 		Member loginUser = memberService.selectLogin(member.getMemId());
-		
+
 		if (loginUser != null && this.bcryptPasswordEncoder.matches(member.getMemPw(), loginUser.getMemPw())) {
 			session.setAttribute("loginUser", loginUser);
 			status.setComplete();
 			return "common/main";
-		} else { 
+		} else {
 			model.addAttribute("message", "로그인에 실패하였습니다. 아이디와 비밀번호를 확인 후 다시 시도해주시기 바랍니다.");
 			return "common/error";
 		}
 	}
-	
+
 	// 회원가입 기능 | 2024. 09. 28 작성 및 테스트 성공
-	@RequestMapping(value = "enroll.do", method=RequestMethod.POST)
+	@RequestMapping(value = "enroll.do", method = RequestMethod.POST)
 	public String enrollMethod(Member member, Model model, HttpServletRequest request) {
 		logger.info("enroll.do : " + member);
-		
+
 		member.setMemPw(bcryptPasswordEncoder.encode(member.getMemPw()));
 		logger.info("after encode : " + member.getMemPw() + ", length : " + member.getMemPw().length());
-		
+
 		if (memberService.insertMember(member) > 0) {
 			return "member/loginPage";
 		} else {
@@ -141,12 +143,12 @@ public class MemberController {
 			return "common/error";
 		}
 	}
-	
+
 	// 로그아웃 기능 | 2024. 09. 28 작성 및 테스트 성공
 	@RequestMapping("logout.do")
 	public String logoutMethod(HttpServletRequest request, Model model) {
 		HttpSession session = request.getSession(false);
-		
+
 		if (session != null) {
 			session.invalidate();
 			return "common/main";
@@ -155,47 +157,45 @@ public class MemberController {
 			return "common/error";
 		}
 	} // 로그아웃 기능
-	
+
 	// 아이디 중복 검사 기능 | 2024. 09. 28 수정 필요
-	@RequestMapping(value="idchk.do", method=RequestMethod.POST)
+	@RequestMapping(value = "idchk.do", method = RequestMethod.POST)
 	@ResponseBody
-	public void dupCheckIdMethod(@RequestParam("memId") String memId, 
-			HttpServletResponse response) throws IOException {
+	public void dupCheckIdMethod(@RequestParam("memId") String memId, HttpServletResponse response) throws IOException {
 		int userIdCount = memberService.selectCheckId(memId);
 		String returnStr = null;
-		if(userIdCount == 0) {
+		if (userIdCount == 0) {
 			returnStr = "ok";
-		}else {
+		} else {
 			returnStr = "dup";
 		}
-		
+
 		response.setContentType("text/html; charset=utf-8");
 		PrintWriter out = response.getWriter();
 		out.append(returnStr);
 		out.flush();
 		out.close();
 	} // 아이디 중복 검사 기능
-	
+
 	// 아이디 찾기 기능
-	@RequestMapping(value="idSearch.do", method=RequestMethod.POST)
+	@RequestMapping(value = "idSearch.do", method = RequestMethod.POST)
 	public String idSearchMethod() {
-		return "member/idSearch";		
+		return "member/idSearch";
 	} // 아이디 찾기 기능
-	
+
 	// 비밀번호 찾기 기능
-	@RequestMapping(value="pwSearch.do", method=RequestMethod.POST)
+	@RequestMapping(value = "pwSearch.do", method = RequestMethod.POST)
 	public String pwSearchMethod() {
-		return "member/pwSearch";		
+		return "member/pwSearch";
 	} // 비밀번호 찾기 기능
 
 	// 내 정보 수정 기능
-	@RequestMapping(value="myinfo/update.do", method=RequestMethod.POST)
-	public String myinfoUpdateMethod(Member member, Model model, 
-			HttpServletRequest request,
+	@RequestMapping(value = "myinfo/update.do", method = RequestMethod.POST)
+	public String myinfoUpdateMethod(Member member, Model model, HttpServletRequest request,
 			@RequestParam("originalMemPw") String originalMemPw) {
-		
+
 		logger.info("myinfo/update.do : " + member);
-		
+
 		// 내 정보 수정에서 회원이 비밀번호를 변경한 경우 변경한 요청에 따라 비밀번호를 변경하는 기능
 		if (member.getMemPw() != null && member.getMemPw().length() > 0) {
 			member.setMemPw(bcryptPasswordEncoder.encode(member.getMemPw()));
@@ -203,7 +203,7 @@ public class MemberController {
 		} else {
 			member.setMemPw(originalMemPw);
 		}
-		
+
 		// 내 정보 수정 요청 성공여부
 		if (memberService.updateMyinfo(member) > 0) {
 			return "redirect:../main.do";
@@ -212,33 +212,33 @@ public class MemberController {
 			return "common/error";
 		}
 	} // 내 정보 수정 기능
-	
+
 	// 회원 탈퇴 기능
-	@RequestMapping(value="myinfo/left.do")
+	@RequestMapping(value = "myinfo/left.do")
 	public String memberLeftMethod(@RequestParam("memId") String memId, Model model) {
-		
-		if(memberService.updateLeft(memId) > 0) {			
+
+		if (memberService.updateLeft(memId) > 0) {
 			return "redirect:../logout.do";
 		} else {
 			model.addAttribute("message", "회원탈퇴에 실패하였습니다.");
 			return "common/error";
 		}
 	} // 회원 탈퇴 기능
-	
+
 	// 관리자 : 회원 정보 수정 기능
-	@RequestMapping(value="admin/memberUpdate.do", method=RequestMethod.POST)
+	@RequestMapping(value = "admin/memberUpdate.do", method = RequestMethod.POST)
 	public String memberUpdateMethod() {
 		return "admin/memberDetail.do";
 	} // 관리자 : 회원 정보 수정 기능
-	
+
 	// 관리자 : 회원 계정 조치 기능
-	@RequestMapping(value="admin/memberAccountUpdate.do", method=RequestMethod.POST)
+	@RequestMapping(value = "admin/memberAccountUpdate.do", method = RequestMethod.POST)
 	public String memberAccountUpdateMethod() {
-		return "admin/memberDetail.do";		
+		return "admin/memberDetail.do";
 	} // 관리자 : 회원 계정 조치 기능
-	
+
 	// 관리자 : 회원 관리자 부여 기능
-	@RequestMapping(value="admin/memberAdmin.do", method=RequestMethod.POST)
+	@RequestMapping(value = "admin/memberAdmin.do", method = RequestMethod.POST)
 	public String memberAdminMethod() {
 		return "admin/memberDetail.do";
 	} // 관리자 : 회원 관리자 부여 기능
